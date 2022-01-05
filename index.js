12ed116c13e8bd33dfb2371293073bef61b1c089
unknown <montasirazad@gmail.com>
2022-01-05 10:23:02 +0600

    error

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+app.use(cors());
+app.use(bodyParser.json());
+
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./authentication-all-practice-firebase-adminsdk-wbs5e-2689df8719.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://azad:azad1403@cluster0.wov4a.mongodb.net/burj-al-arab?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("burj-al-arab").collection("bookings");
+  console.log('db connected');
+
+
+  app.post('/addBooking', (req, res) => {
+    const newBooking = req.body;
+    collection.insertOne(newBooking)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+  })
+
+  app.get('/bookings', (req, res) => {
+    const bearer = req.headers.authorization;
+    if (bearer && bearer.startsWith('Bearer')) {
+      const idToken = bearer.split(' ');
+      console.log({ idToken });
+
+    }
+
+    // idToken comes from the client app
+    getAuth()
+      .verifyIdToken(idToken)
+      .then((decodedToken) => {
+        const tokenEmail = decodedToken.email;
+        const queryEmail = req.query.email;
+        console.log(tokenEmail, queryEmail);
+        if (tokenEmail == req.query.email) {
+          collection.find({ email: req.query.email })
+            .toArray((err, documents) => {
+              res.send(documents);
+            })
+        }
+        else {
+          res.status(401).send('unauthorized')
+        }
+
+      })
+      .catch((error) => {
+        // Handle error
+      });
+
+
+
+  })
+
+
+});
+
+
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(5000, () => {
+  console.log('listening to port 5000');
+})
